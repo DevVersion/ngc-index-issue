6e9ec09a9461b5a2047a577365fe91e9d4099105
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-05-06 16:08:38 +0200

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-import {NgModule} from '@angular/core';
+import {Component, NgModule} from '@angular/core';
 export {MyTestConstant} from './my-exports';
 
-@NgModule({})
+@Component({
+  template: '<span>Hey</span>'
+})
+export class TestComponent {}
+
+@NgModule({
+  declarations: [TestComponent],
+  exports: [TestComponent]
+})
 export class TestModule {}
